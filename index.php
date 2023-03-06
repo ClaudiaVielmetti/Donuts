0660e8363dfbe46d93a9ff4c7ba19e52f9080fac
ClaudiaV27 <claudiavielmetti38@gmail.com>
2023-03-06 15:42:13 +0200

    updates

--- index.php
+++ index.php
@@ -1,37 +1,104 @@
-<?php
-  // Initialize session
-  session_start();
-
-  // Redirect to order page if user is already logged in
-  if (isset($_SESSION['username'])) {
-    header('Location: order.php');
-    exit;
-  }
-?>
+<!-- index.php -->
 <!DOCTYPE html>
 <html>
-  <head>
-    <title>Dropping Donuts - Home</title>
-    <link rel="stylesheet" href="assets/css/style.css">
-  </head>
-  <body>
-    <header>
-      <h1>Dropping Donuts</h1>
-      <nav>
-        <ul>
-          <li><a href="index.php">Home</a></li>
-          <li><a href="order.php">Order</a></li>
-          <li><a href="login.php">Login</a></li>
-        </ul>
-      </nav>
-    </header>
-    <main>
-      <h2>Welcome to Dropping Donuts!</h2>
-      <p>Our donuts are freshly made every day using only the finest ingredients. We offer a variety of flavors and toppings, but if you don't see what you want on the menu, you can create your own donut!</p>
-      <p>Click the "Order" link above to get started.</p>
-    </main>
-    <footer>
-      <p>&copy; 2023 Dropping Donuts</p>
-    </footer>
-  </body>
+
+<head>
+    <title>Dropping Donuts Order Form</title>
+</head>
+
+<body>
+    <h1>Dropping Donuts Order Form</h1>
+    <form method="post" action="payment.php">
+        <!-- username input -->
+        <label for="username">Username:</label>
+        <input type="text" id="username" name="username" required><br><br>
+
+        <!-- glaze input -->
+        <label>Glaze:</label><br>
+        <input type="radio" id="plain" name="glaze" value="plain" checked>
+        <label for="plain">Plain</label><br>
+        <input type="radio" id="chocolate" name="glaze" value="chocolate">
+        <label for="chocolate">Chocolate</label><br>
+        <input type="radio" id="strawberry" name="glaze" value="strawberry">
+        <label for="strawberry">Strawberry</label><br><br>
+
+        <!-- toppings input -->
+        <label>Toppings (max 3):</label><br>
+        <input type="checkbox" id="sprinkles" name="toppings[]" value="sprinkles">
+        <label for="sprinkles">Sprinkles</label><br>
+        <input type="checkbox" id="nuts" name="toppings[]" value="nuts">
+        <label for="nuts">Nuts</label><br>
+        <input type="checkbox" id="fruit" name="toppings[]" value="fruit">
+        <label for="fruit">Fruit</label><br><br>
+
+        <!-- filling input -->
+        <label>Filling:</label><br>
+        <select id="filling" name="filling">
+            <option value="none" selected>None</option>
+            <option value="chocolate">Chocolate</option>
+            <option value="caramel">Caramel</option>
+            <option value="jam">Jam</option>
+        </select><br><br>
+
+        <!-- number of donuts input -->
+        <label for="quantity">Quantity:</label>
+        <input type="number" id="quantity" name="quantity" min="1" value="1" required><br><br>
+
+        <!-- submit button -->
+        <input type="submit" value="Submit">
+    </form>
+</body>
+
 </html>
+
+<?php
+// payment.php
+
+// function to calculate the price of a single donut
+function calcPriceOfDonut($glaze, $toppings, $filling)
+{
+    $price = 4.0; // base price
+    if ($glaze != 'plain') {
+        $price += 2.0; // additional price for glaze
+    }
+    $price += count($toppings) * 2.0; // additional price for toppings
+    if ($filling != 'none') {
+        $price += 2.0; // additional price for filling
+    }
+    return $price;
+}
+
+// function to calculate the total order price
+function getOrderTotal($priceOfDonut, $quantity)
+{
+    return $priceOfDonut * $quantity;
+}
+
+// check if the form was submitted
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    // retrieve theform data
+    $username = $_POST['username'];
+    $glaze = $_POST['glaze'];
+    $toppings = isset($_POST['toppings']) ? $_POST['toppings'] : array();
+    $filling = $_POST['filling'];
+    $quantity = $_POST['quantity']; // calculate the price of a single donut
+    $priceOfDonut = calcPriceOfDonut($glaze, $toppings, $filling);
+
+    // calculate the total order price
+    $orderTotal = getOrderTotal($priceOfDonut, $quantity);
+
+    // display the order summary
+    echo "<h2>Order Summary</h2>";
+    echo "<p>Username: $username</p>";
+    echo "<p>Glaze: $glaze</p>";
+    echo "<p>Toppings: " . implode(", ", $toppings) . "</p>";
+    echo "<p>Filling: $filling</p>";
+    echo "<p>Quantity: $quantity</p>";
+    echo "<p>Price per donut: $" . number_format($priceOfDonut, 2) . "</p>";
+    echo "<p>Total price: $" . number_format($orderTotal, 2) . "</p>";
+} else {
+    // if the form was not submitted, redirect to the order form page
+    header('Location: index.php');
+    exit();
+}
+?>
